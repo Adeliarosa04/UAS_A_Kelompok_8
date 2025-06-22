df66eaebdeaa265b6fe1feda9bf3a5b83a4bf30e
fahmavella <fahmazoeyya@student.uns.ac.id>
2025-06-22 22:36:45 +0700

    interface proses produksi lagi

--- toko roti Hanari.py
+++ toko roti Hanari.py
@@ -1,1 +1,32 @@
-toko roti Hanari.py
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+# ==============================================
+# ANTARMUKA UNTUK PROSES PRODUKSI (Prinsip SOLID)
+# ==============================================
+
+#interface untuk proses produksi bakery
+class ProsesProduksi(ABC):
+    @abstractmethod
+    def aduk(self) -> str:
+        pass
+    
+    @abstractmethod
+    def panggang(self) -> str:
+        pass
+    
+    @abstractmethod
+    def kemas(self) -> str:
+        pass
+
+#interface khusus untuk produk yang perlu pengembangan
+class ProsesPengembangan(ABC):
+    @abstractmethod
+    def kembangkan(self) -> str:
+        pass
+
+#Interface khusus untuk produk dengan topping
+class ProsesTopping(ABC):
+    @abstractmethod
+    def beri_topping(self) -> str:
+        pass
